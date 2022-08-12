a43cdf84117aed4425b2bf25c0e01ca07bcd67d9
evanleeturner <evanlee.turner@gmail.com>
2022-08-12 10:06:55 -0500

    progress

--- download_winds.py
+++ download_winds.py
@@ -26,9 +26,36 @@ def download_latest():
     logging.debug("Changing directory to downloaded_data/latest ")
     os.chdir('downloaded_data/latest')
 
+
+
+    logging.debug("Fetching html for {url_base}".format(url_base=url_base))
+    html_buff = pd.read_html(url_base)  #download the base page for forecast months
+    monthly_forecasts= html_buff[0]
+    monthly_forecasts.dropna(how='all', inplace=True)
+    our_month = monthly_forecasts.iloc[-1,0]  #grabs the very last forecast month..  eg. '202208/'
+    logging.debug("Latest month folder: {our_month}".format(our_month=our_month))
+    #repeat procedure for latest forecast of the month...
+    logging.debug("Fetching html for {url}".format(url=our_month))
+    html_buff = pd.read_html(url_base+our_month)  #download the base page for forecast months
+    daily_forecasts= html_buff[0]
+    daily_forecasts.dropna(how='all', inplace=True)
+    our_day = daily_forecasts.iloc[-1,0]  #grabs the very last forecast day..  eg. '20220806/'
+    logging.debug("Latest day folder: {our_day}".format(our_day=our_day))
+    #repeat to find the last run hourly forecast in the list..
+    logging.debug("Fetching html for {url}".format(url=our_day))
+    html_buff = pd.read_html(url_base+our_month+our_day)
+    hourly_forecasts= html_buff[0]
+    hourly_forecasts.dropna(how='all', inplace=True)
+    last_file = hourly_forecasts.iloc[-1,0]
+    logging.debug("Latest file in list: {last_file}".format(last_file=last_file))
+    file_prefix = last_file[:-13]  #boil down the last file name to just the file prefix, etc. 'nam_218_20220806_1800'
+
+    file_post = '.grb2'
+
+    #logic to see if we need to clear out old files...
     files = glob.glob('*.grb2')
     if files:
-        logging.debug("Found files for deletion: {files}".format(files=files))
+        logging.debug("Found grb2 files for deletion: {files}".format(files=files))
         for f in files:
             try:
                 os.remove(f)
@@ -37,31 +64,7 @@ def download_latest():
                 print("Error: %s : %s" % (f, e.strerror))
 
 
-    logging.debug("Fetching html for {url_base}".format(url_base=url_base))
-    html_buff = pd.read_html(url_base)  #download the base page for forecast months
-    monthly_forecasts= html_buff[0]
-    monthly_forecasts.dropna(how='all', inplace=True)
-    our_month = monthly_forecasts.iloc[-1,0]  #grabs the very last forecast month..  eg. '202208/'
-    #repeat procedure for latest forecast of the month...
-    logging.debug("Fetching html for {url}".format(url=our_month))
-    html_buff = pd.read_html(url_base+our_month)  #download the base page for forecast months
-    daily_forecasts= html_buff[0]
-    daily_forecasts.dropna(how='all', inplace=True)
-    our_day = daily_forecasts.iloc[-1,0]  #grabs the very last forecast day..  eg. '20220806/'
-    #repeat to find the last run hourly forecast in the list..
-    logging.debug("Fetching html for {url}".format(url=our_day))
-    html_buff = pd.read_html(url_base+our_month+our_day)
-    hourly_forecasts= html_buff[0]
-    hourly_forecasts.dropna(how='all', inplace=True)
-    last_file = hourly_forecasts.iloc[-1,0]
-    file_prefix = last_file[:-13]  #boil down the last file name to just the file prefix, etc. 'nam_218_20220806_1800'
-
-
-
-    file_post = '.grb2'
-
     st = time.time()  #our program start time...
-
     #download all of the grib files... this could take a while
     index = 0
     for i in range(53):
@@ -79,10 +82,10 @@ def download_latest():
         else:
             index+=3
 
-
     ft = time.time()
     elapsed_time = et - st
     logging.info('Total download took: {elapsed_time} seconds'.format(elapsed_time=elapsed_time))
 
     logging.debug("Exiting function download_latest() successfully")
+
 download_latest()
